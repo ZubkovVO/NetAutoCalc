3b8d68c49f9c6a503d5571fae31cbc6328930991
Виталий Зубков <nrcos@mail.ru>
2020-03-17 00:01:05 +0300

    Code cleanup, put all operations in one class MathematicalOperations, added switch case statement for operations

--- Calc.cs
+++ Calc.cs
@@ -12,80 +12,35 @@ namespace Calc
             Console.WriteLine("Please input the desired equation (+-/*) using the following format: n O n, where 'n' stand for number and 'O' for operation e.g 3 + 4");
             string equationInput = Console.ReadLine();
             string[] eq = { equationInput };
-            //string[] eq = input.Split(' ');   Массив
+            string n1 = eq[0];
 
-            /*foreach (var number in eq)
-            {
-                Console.WriteLine($"<{number}>");
-            }
-            var checkArr = eq.GetLength(0);
-            Console.WriteLine(checkArr);*/
-
-            /*if (checkArr > 3)
-            {
-                Console.WriteLine("Incorrect number of parameters - " + checkArr + " Please refer to the example: 1 + 1");
-            }
-            else
-            {*/
-                string n1 = eq[0];
-                // string o1 = eq[1];
-                // string n2 = eq[2];
-                Console.WriteLine($"{n1} {(isValidNumber(n1) ? "is" : "is not")} a valid equation.");
-            //Console.WriteLine("{0} {1} a valid operator.",o1, isValidOperator(o1) ? "is" : "is not");
-            // Console.WriteLine("{0} {1} a valid number.",n2, isValidNumber(n2) ? "is" : "is not");
-
-            //};
+            Console.WriteLine($"{n1} {(isValidEquation(n1) ? "is" : "is not")} a valid equation.");
 
             eq = equationInput.Split(' ');
-            foreach (var number in eq)
+            switch (eq[1])
             {
-                Console.WriteLine($"<{number}>");
+                case "+":
+                    Console.WriteLine(MathematicalOperations.Add(Int16.Parse(eq[0]), Int16.Parse(eq[2])));
+                    break;
+                case "-":
+                    Console.WriteLine(MathematicalOperations.Substract(Int16.Parse(eq[0]), Int16.Parse(eq[2])));
+                    break;
+
+                case "*":
+                    Console.WriteLine(MathematicalOperations.Multiply(Int16.Parse(eq[0]), Int16.Parse(eq[2])));
+                    break;
+                case "/":
+                    Console.WriteLine(MathematicalOperations.Divide(Int16.Parse(eq[0]), Int16.Parse(eq[2])));
+                    break;
             }
-            var checkArr = eq.GetLength(0);
-            Console.WriteLine(checkArr);
-
-            if (eq[1] == "+")
-            {
-               Addition sum = new Addition();
-                sum.Add(Int16.Parse(eq[0]), Int16.Parse(eq[2]));
-                Console.WriteLine(sum);
-            }
-
-
-
-
-            //int.TryParse(number1, out var newVariable);
-
-            //Console.ReadKey();
-
-
 
         }
 
-        public static bool isValidNumber(string inputEquation)
+        public static bool isValidEquation(string inputEquation)
         {
             string strRegex = @"\d+\s[*+/-]\s\d+";
-
-            //@"^((?:((-?\d+)(,\d+)?)+[*+/-])+((-?\d+)(,\d+)?)+)|^(-?\d+)(\.\d+)?$";
-
-            // 
-
             Regex re = new Regex(strRegex);
-
             return re.IsMatch(inputEquation);
-
-        }
-
-        public static bool isValidOperator(string inputEquation)
-        {
-            //string strRegex = @"[-*\+]";
-
-            Regex re = new Regex(@"[-*+/]" + "[0-9]");
-
-            if (re.IsMatch(inputEquation))
-                return (true);
-            else
-                return (false);
         }
 
     }
---
+++ MathematicalOperations.cs
@@ -0,0 +1,33 @@
+namespace Calc
+{
+
+    class MathematicalOperations
+    {
+
+        public static int Add(int a, int b)
+        {
+            var sum = a + b;
+            return sum;
+
+        }
+
+        public static int Divide(int a, int b)
+        {
+            var div = a / b;
+            return div;
+
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            var mult = a * b;
+            return mult;
+        }
+
+        public static int Substract(int a, int b)
+        {
+            var subs = a - b;
+            return subs;
+        }
+    }
+}
