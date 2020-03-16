fe97224e13497ed47ad8b72222f870953befb9d3
Виталий Зубков <nrcos@mail.ru>
2020-03-16 09:58:26 +0300

    test connection with methods

--- Calc.cs
+++ Calc.cs
@@ -14,6 +14,29 @@ namespace Calc
             string[] eq = { equationInput };
             //string[] eq = input.Split(' ');   Массив
 
+            /*foreach (var number in eq)
+            {
+                Console.WriteLine($"<{number}>");
+            }
+            var checkArr = eq.GetLength(0);
+            Console.WriteLine(checkArr);*/
+
+            /*if (checkArr > 3)
+            {
+                Console.WriteLine("Incorrect number of parameters - " + checkArr + " Please refer to the example: 1 + 1");
+            }
+            else
+            {*/
+                string n1 = eq[0];
+                // string o1 = eq[1];
+                // string n2 = eq[2];
+                Console.WriteLine($"{n1} {(isValidNumber(n1) ? "is" : "is not")} a valid equation.");
+            //Console.WriteLine("{0} {1} a valid operator.",o1, isValidOperator(o1) ? "is" : "is not");
+            // Console.WriteLine("{0} {1} a valid number.",n2, isValidNumber(n2) ? "is" : "is not");
+
+            //};
+
+            eq = equationInput.Split(' ');
             foreach (var number in eq)
             {
                 Console.WriteLine($"<{number}>");
@@ -21,21 +44,13 @@ namespace Calc
             var checkArr = eq.GetLength(0);
             Console.WriteLine(checkArr);
 
-            if (checkArr > 3)
+            if (eq[1] == "+")
             {
-                Console.WriteLine("Incorrect number of parameters - " + checkArr + " Please refer to the example: 1 + 1");
+               Addition sum = new Addition();
+                sum.Add(Int16.Parse(eq[0]), Int16.Parse(eq[2]));
+                Console.WriteLine(sum);
             }
-            else
-            {
-                string n1 = eq[0];
-                // string o1 = eq[1];
-                // string n2 = eq[2];
 
-                Console.WriteLine($"{n1} {(isValidNumber(n1) ? "is" : "is not")} a valid number.");
-                //Console.WriteLine("{0} {1} a valid operator.",o1, isValidOperator(o1) ? "is" : "is not");
-                // Console.WriteLine("{0} {1} a valid number.",n2, isValidNumber(n2) ? "is" : "is not");
-
-            };
 
 
 
