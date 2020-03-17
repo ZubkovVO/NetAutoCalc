6b064a7ce4f3a74393b4b2367aa137a2dd77d7ee
Виталий Зубков <nrcos@mail.ru>
2020-03-17 20:02:32 +0300

    Minor code cleanup, validation regex moved to class Validation

--- Calc.cs
+++ Calc.cs
@@ -1,7 +1,6 @@
 using System;
-using System.Text.RegularExpressions;
 
-//Replace()
+
 
 namespace Calc
 {
@@ -12,35 +11,35 @@ namespace Calc
             Console.WriteLine("Please input the desired equation (+-/*) using the following format: n O n, where 'n' stand for number and 'O' for operation e.g 3 + 4");
             string equationInput = Console.ReadLine();
             string[] eq = { equationInput };
-            string n1 = eq[0];
 
-            Console.WriteLine($"{n1} {(isValidEquation(n1) ? "is" : "is not")} a valid equation.");
-
-            eq = equationInput.Split(' ');
-            switch (eq[1])
+            if (Validation.isValidEquation(eq[0]) == true)
             {
-                case "+":
-                    Console.WriteLine(MathematicalOperations.Add(Int16.Parse(eq[0]), Int16.Parse(eq[2])));
-                    break;
-                case "-":
-                    Console.WriteLine(MathematicalOperations.Substract(Int16.Parse(eq[0]), Int16.Parse(eq[2])));
-                    break;
+                Console.WriteLine($"'{eq[0]}' is a valid equation, starting calcualtion process bzzzz");
+                eq = equationInput.Split(' ');
+                switch (eq[1])
+                {
+                    case "+":
+                        Console.WriteLine(MathematicalOperations.Add(Int16.Parse(eq[0]), Int16.Parse(eq[2])));
+                        break;
+                    case "-":
+                        Console.WriteLine(MathematicalOperations.Substract(Int16.Parse(eq[0]), Int16.Parse(eq[2])));
+                        break;
 
-                case "*":
-                    Console.WriteLine(MathematicalOperations.Multiply(Int16.Parse(eq[0]), Int16.Parse(eq[2])));
-                    break;
-                case "/":
-                    Console.WriteLine(MathematicalOperations.Divide(Int16.Parse(eq[0]), Int16.Parse(eq[2])));
-                    break;
+                    case "*":
+                        Console.WriteLine(MathematicalOperations.Multiply(Int16.Parse(eq[0]), Int16.Parse(eq[2])));
+                        break;
+                    case "/":
+                        Console.WriteLine(MathematicalOperations.Divide(Int16.Parse(eq[0]), Int16.Parse(eq[2])));
+                        break;
+                }
+
             }
 
-        }
+            else
+            {
+                Console.WriteLine($"'{eq[0]}' is not a valid equation, try again.");
+            }
 
-        public static bool isValidEquation(string inputEquation)
-        {
-            string strRegex = @"\d+\s[*+/-]\s\d+";
-            Regex re = new Regex(strRegex);
-            return re.IsMatch(inputEquation);
         }
 
     }
---
+++ Validation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Validation
+{
+
+public static bool isValidEquation(string inputEquation)
+{
+    string strRegex = @"\d+\s[*+/-]\s\d+";
+    Regex re = new Regex(strRegex);
+    return re.IsMatch(inputEquation);
+}
+
+}
